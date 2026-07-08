9aa892289effbdb539e28ff2edb05d1f9df0b195
numpy1234 <dikshantkumar312@gmail.com>
2026-07-08 23:59:15 +0530

    Add BCD to Excess-3 converter module

---
+++ design/design.sv
@@ -0,0 +1,8 @@
+module bcd_to_excess3(a,b,c,d,w,x,y,z);
+  input a,b,c,d;
+  output w,x,y,z;
+  assign z=~d;
+  assign y=(c&d)|(~(c+d));
+  assign x=(~b&c)|(~b&d)|(b&~c&~d);
+  assign w=a|(b&c)|(b&d);
+endmodule
